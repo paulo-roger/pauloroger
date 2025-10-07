11e889833da51f5a67b0866d020432bacd4a130c
Paulo Roger <me@pauloroger.dev>
2025-10-07 17:57:57 -0300

    added postcss package for min css size

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+
+const purgecss = purgeCSSPlugin({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  safelist: [],
+});
+
+export default {
+  plugins: [
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
+};
